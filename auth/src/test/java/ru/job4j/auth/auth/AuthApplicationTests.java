35204145af77ffc8bd2692cfcff9cc39cbcf3f09
Aliaksandr Zelianko <zely20@mail.ru>
2021-08-30 15:26:36 +0300

    add test

---
+++ auth/src/test/java/ru/job4j/auth/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.auth.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
