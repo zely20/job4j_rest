a3c3e51d37897834b5037f946e84b7d287898994
Aliaksandr Zelianko <zely20@mail.ru>
2021-08-30 16:09:35 +0300

    add test

--- auth/src/main/java/ru/job4j/auth/auth/AuthApplication.java
+++ auth/src/main/java/ru/job4j/auth/auth/AuthApplication.java
@@ -1,13 +1,25 @@
 package ru.job4j.auth.auth;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class AuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
     }
 
 }
